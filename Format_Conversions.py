febbbbd003c8d8346b3deb8e5bc175e76e903e80
iljohnson <ian101@thejohnsonsplace.org>
2026-01-14 19:59:57 +1100

    Delete Format_Conversions.py
    not needed as its now included in another module

--- Format_Conversions.py
+++
@@ -1,7 +0,0 @@
-
-def convert_strings_to_floats(input_array):
-    output_array = []
-    for element in input_array:
-        converted_float = float(element)
-        output_array.append(converted_float)
-    return output_array
