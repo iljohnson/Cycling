5fdeef681da8be60eaf7b54c748175877fbcdb76
iljohnson <ian101@thejohnsonsplace.org>
2026-01-14 20:02:28 +1100

    Create my_global_variables.py
    this module holds the global variables

---
+++ my_global_variables.py
@@ -0,0 +1,6 @@
+#  These are the global variables used
+
+
+file_name = ""  # this variable holds the file name and location for the saved file
+
+z = ()  # this holds the dataframe which is has teh initial data and the updated data saved to excel
